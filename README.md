082affb5364735def0c9703150145cfe0c0f6d6c
JeanChanning <tmy.1225@foxmail.com>
2019-04-26 09:35:18 +0800

    fix search

--- README.md
+++ README.md
@@ -14,23 +14,27 @@ https://jeanchanning.github.io/WebAssignment_2/AnotherMovieIndex.html
 
 AnotherMovieIndex.html
 
-每页显示20条电影记录，共10页，已实现翻页功能。其中部分图片存在无法加载的问题，与豆瓣服务器有关
+每页显示20条电影记录，共10页，已实现翻页功能和电影名称模糊搜索功能。其中部分图片存在无法加载的问题，与豆瓣服务器有关
 
 ##### 第一页
 
-![1556187320103](C:\Users\Teng_MY\AppData\Roaming\Typora\typora-user-images\1556187320103.png)
+![1556241302477](C:\Users\Teng_MY\AppData\Roaming\Typora\typora-user-images\1556241302477.png)
 
-![1556187341740](C:\Users\Teng_MY\AppData\Roaming\Typora\typora-user-images\1556187341740.png)
+![1556241320300](C:\Users\Teng_MY\AppData\Roaming\Typora\typora-user-images\1556241320300.png)
+
+![1556241363390](C:\Users\Teng_MY\AppData\Roaming\Typora\typora-user-images\1556241363390.png)
 
 ##### 其他页（第7页为例）
 
-![1556187384336](C:\Users\Teng_MY\AppData\Roaming\Typora\typora-user-images\1556187384336.png)
+![1556241374748](C:\Users\Teng_MY\AppData\Roaming\Typora\typora-user-images\1556241374748.png)
 
 ##### 页面底部推荐
 
-![1556187440667](C:\Users\Teng_MY\AppData\Roaming\Typora\typora-user-images\1556187440667.png)
+![1556241396373](C:\Users\Teng_MY\AppData\Roaming\Typora\typora-user-images\1556241396373.png)
 
 #### 详情页
+
+MovieDetail.html
 
 首页中，点击列表中电影名称，或点击最下方的推荐电影，即可跳转到详情页面。点击左上角返回榜单即可返回首页列表
 
@@ -39,3 +43,6 @@ AnotherMovieIndex.html
 ![1556187595261](C:\Users\Teng_MY\AppData\Roaming\Typora\typora-user-images\1556187595261.png)
 
 ![1556187608283](C:\Users\Teng_MY\AppData\Roaming\Typora\typora-user-images\1556187608283.png)
+
+#### 搜索页
+
--- js/search_byname.js
+++ js/search_byname.js
@@ -14,81 +14,80 @@ function searchClick(){
 
 
 let name = getParams("name");
+document.getElementById("searchresult").innerHTML = "<h2>搜索结果如下</h2>"
 if(name == null || name == ""){
     document.getElementById("searchresult").innerHTML = "<h2>请输入电影的名字……</h2>"
-}
-document.getElementById("searchresult").innerHTML = "<h2>搜索结果如下</h2>"
+}else {
+    for (var i = 1; i <= 200; i++) {
+        let requestURL = 'index/' + i + '.json';
 
+        let request = new XMLHttpRequest();
+        request.open('GET', requestURL);
+        request.responseType = 'text';
+        request.send();
 
-for(var i = 1; i <= 200; i++){
-    let requestURL = 'index/' + i + '.json';
+        request.onload = function () {
+            let responseText = request.response;
+            let text = JSON.parse(responseText);
 
-    let request = new XMLHttpRequest();
-    request.open('GET',requestURL);
-    request.responseType = 'text';
-    request.send();
-
-    request.onload = function(){
-        let responseText = request.response;
-        let text = JSON.parse(responseText);
-
-        var title = text.title;
-        index = title.indexOf(name);
-        if(index != -1) {
-            var link = "MovieDetails.html?id=" + text._id;
             var title = text.title;
-            var pubdate = text.pubdate;
-            var average = text.rating.average;
-            var rating = parseFloat(average) * 6;
-            var rating_people = text.rating.rating_people;
-            var poster = text.poster;
-            var summary = text.summary.substring(0, 128) + '……';
-            cs = '';
-            for(var i of text.casts){
-                cs += i.name + ' / ';
+            index = title.indexOf(name);
+            if (index != -1) {
+                var link = "MovieDetails.html?id=" + text._id;
+                var title = text.title;
+                var pubdate = text.pubdate;
+                var average = text.rating.average;
+                var rating = parseFloat(average) * 6;
+                var rating_people = text.rating.rating_people;
+                var poster = text.poster;
+                var summary = text.summary.substring(0, 128) + '……';
+                cs = '';
+                for (var i of text.casts) {
+                    cs += i.name + ' / ';
+                }
+                var casts = cs.substring(0, cs.length - 3);
+
+                var tableObj = document.getElementById("movieTable");
+                tableObj.innerHTML +=
+                    '<td>' +
+                    '    <div style="font-size: 20px" class="row clearfix">\n' +
+                    '        <div class="col-md-3 column">\n' +
+                    '            <img style="width: 210px;height: 308px;" alt="' + title + '" src="' + poster + '" /> \n' +
+                    '        </div>\n' +
+                    '        <div class="col-md-9 column">\n' +
+                    '            <div class="row clearfix">\n' +
+                    '                <div class="col-md-9 column">\n <a href="' + link + '">' + title + '</a>' +
+                    '                </div>\n' +
+                    '                <div class="col-md-3 column">\n' +
+                    '                    <div id="bg">\n' +
+                    '                        <div id="over" style="width:' + rating + 'px"></div>\n' +
+                    '                    </div>' +
+                    '                    <span style="color: #ac2925">' + average + '</span> (' + rating_people + '人已评价)' +
+                    '                </div>\n' +
+                    '            </div>\n' +
+                    '            <div style="font-size: 16px" class="row clearfix">\n' +
+                    '                <div class="col-md-9 column">\n' + pubdate +
+                    '                </div>\n' +
+                    '            </div>' +
+                    '            <div class="row clearfix">\n' +
+
+                    '            </div>\n' +
+                    '            <div class="row clearfix">\n' +
+                    '                <div class="col-md-12 column">\n' + casts +
+                    '                </div>\n' +
+                    '            </div>\n' +
+                    '            <hr />' +
+                    '            <div class="row clearfix">\n' +
+                    '                <div class="col-md-12 column">\n' +
+                    '                    <p>\n' + summary +
+                    '                    </p>\n' +
+                    '                </div>\n' +
+                    '            </div>\n' +
+                    '        </div>\n' +
+                    '    </div>' +
+                    '    <hr class="hr4"/>\n'
+                '</td>'
             }
-            var casts = cs.substring(0, cs.length - 3);
-
-            var tableObj = document.getElementById("movieTable");
-            tableObj.innerHTML +=
-                '<td>' +
-                '    <div style="font-size: 20px" class="row clearfix">\n' +
-                '        <div class="col-md-3 column">\n' +
-                '            <img style="width: 210px;height: 308px;" alt="' + title + '" src="' + poster + '" /> \n' +
-                '        </div>\n' +
-                '        <div class="col-md-9 column">\n' +
-                '            <div class="row clearfix">\n' +
-                '                <div class="col-md-9 column">\n <a href="' + link + '">' + title + '</a>' +
-                '                </div>\n' +
-                '                <div class="col-md-3 column">\n' +
-                '                    <div id="bg">\n' +
-                '                        <div id="over" style="width:' + rating + 'px"></div>\n' +
-                '                    </div>' +
-                '                    <span style="color: #ac2925">' + average + '</span> (' + rating_people + '人已评价)' +
-                '                </div>\n' +
-                '            </div>\n' +
-                '            <div style="font-size: 16px" class="row clearfix">\n' +
-                '                <div class="col-md-9 column">\n' + pubdate +
-                '                </div>\n' +
-                '            </div>' +
-                '            <div class="row clearfix">\n' +
-
-                '            </div>\n' +
-                '            <div class="row clearfix">\n' +
-                '                <div class="col-md-12 column">\n' + casts +
-                '                </div>\n' +
-                '            </div>\n' +
-                '            <hr />' +
-                '            <div class="row clearfix">\n' +
-                '                <div class="col-md-12 column">\n' +
-                '                    <p>\n' + summary +
-                '                    </p>\n' +
-                '                </div>\n' +
-                '            </div>\n' +
-                '        </div>\n' +
-                '    </div>' +
-                '    <hr class="hr4"/>\n'
-            '</td>'
         }
     }
 }
